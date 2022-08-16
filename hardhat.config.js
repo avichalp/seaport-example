7c5b92f1f1a356e9b687c80c7c3cb3f9e3be09d8
avichalp <hi@avichalp.me>
2022-08-16 16:27:01 +0530

    Initial commit

---
+++ hardhat.config.js
@@ -0,0 +1,21 @@
+require("@nomicfoundation/hardhat-toolbox");
+
+/** @type import('hardhat/config').HardhatUserConfig */
+// require("@nomiclabs/hardhat-waffle");
+
+module.exports = {
+  solidity: {
+    version: "0.8.13",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
+  networks: {
+    hardhat: {
+      chainId: 1337
+    }
+  }
+};
